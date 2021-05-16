ddac504d167e4693e0af5f708459828b41e5a885
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-16 19:57:49 +0300

    add constants for field names

--- graphs-spark/src/main/scala/ru.mai.dep810/Helpers.scala
+++ graphs-spark/src/main/scala/ru.mai.dep810/Helpers.scala
@@ -3,10 +3,18 @@ package ru.mai.dep810
 import org.apache.spark.graphx.Edge
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SparkSession}
-import ru.mai.dep810.GraphSpark.VerticeInfo
 
-object Helpers {
+object CsvDataReader {
   private val DATASET_PATH = "./import/dataset/"
+
+  private val ID_FIELD_NAME = "id"
+  private val COUNT_POSTS_FIELD_NAME = "pos"
+  private val COUNT_FOLLOWERS_FIELD_NAME = "flr"
+  private val COUNT_FOLLOWINGS_FIELD_NAME = "flg"
+  private val ENGAGEMENT_GRADE_FIELD_NAME = "eg"
+  private val ENGAGEMENT_RATE_FIELD_NAME = "er"
+  private val FOLLOWERS_GROWTH_PERCENTAGE_FIELD_NAME = "fg"
+  private val OUTSIDERS_PERCENTAGE_FIELD_NAME = "op"
 
   implicit class SparkSessionReader(session: SparkSession){
     def loadDatasetFromCSV(datasetName: String) : RDD[Row]=
@@ -21,11 +29,16 @@ object Helpers {
 
     def loadVerticiesDataset(datasetName: String) =
       (session loadDatasetFromCSV datasetName)
-       .map(row => (row.getAs[Int](0).toLong, VerticeInfo(row.getAs[Int](1),row.getAs[Double](4))))
+       .map(row => (row.getAs[Int](ID_FIELD_NAME).toLong, VertexInfo(row.getAs[Int](COUNT_POSTS_FIELD_NAME),
+         row.getAs[Int](COUNT_FOLLOWERS_FIELD_NAME),
+         row.getAs[Int](COUNT_FOLLOWINGS_FIELD_NAME),
+         row.getAs[Double](ENGAGEMENT_GRADE_FIELD_NAME),
+         row.getAs[Double](ENGAGEMENT_RATE_FIELD_NAME),
+         row.getAs[Double](FOLLOWERS_GROWTH_PERCENTAGE_FIELD_NAME),
+         row.getAs[Double](OUTSIDERS_PERCENTAGE_FIELD_NAME))))
 
     def loadEdgesDataset(datasetName: String) =
       (session loadDatasetFromCSV datasetName)
         .map(row => Edge(row.getAs[Int](0),row.getAs[Int](1), row.getAs[Double](2)))
-
   }
 }

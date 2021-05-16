874b5c15edec3175f58ebf3edfc8166e6bfb9116
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-16 20:00:01 +0300

    implement task solution prototype

--- graphs-spark/src/main/scala/ru.mai.dep810/Main.scala
+++ graphs-spark/src/main/scala/ru.mai.dep810/Main.scala
@@ -1,14 +1,32 @@
 package ru.mai.dep810
 
-import org.apache.spark.graphx.Graph
-import ru.mai.dep810.GraphSpark.VerticeInfo
-import Helpers._
+import org.apache.spark.graphx.{Graph, VertexRDD}
+import ru.mai.dep810.CsvDataReader._
 
 object Main extends App{
   val sparkSession = GraphSpark.InitSession()
   val verticesRdd = sparkSession loadVerticiesDataset  "instagram_data"
   val edgesRdd = sparkSession loadEdgesDataset  "edges"
 
-  val graph: Graph[VerticeInfo, Double] = Graph(verticesRdd, edgesRdd)
-  graph.inDegrees.take(10).foreach(println)
+  val graph: Graph[VertexInfo, Double] = Graph(verticesRdd, edgesRdd)
+  println("In Degrees:")
+  graph.inDegrees
+    .sortBy(-_._2)
+    .take(10)
+    .foreach(println)
+  println("Out Degrees:")
+  graph.outDegrees
+    .sortBy(-_._2)
+    .take(10)
+    .foreach(println)
+
+  val furtherFollowees: VertexRDD[(Int, Double)] = graph.aggregateMessages[(Int, Double)](
+    triplet => triplet.sendToSrc((triplet.srcId.toInt, triplet.srcAttr.calculateMeasure())),
+    (a, b) => (a._1,a._2 + b._2 ))
+
+  furtherFollowees
+    .sortBy(-_._2._2)
+    .take(10)
+    .foreach(println)
+
 }
